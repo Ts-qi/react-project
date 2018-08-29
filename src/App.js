dfba870d6ed229a956a01f2a17024d1b59af5812
tangqi <1024505996@qq.com>
2018-08-29 23:00:56 +0800

    upload react-transition-group

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 class App extends Component {
+  componentDidMount() {
+    console.log(axios)
+    axios.post('  https://easy-mock.com/mock/5a34accca68e2224f9e92bfb/api/todolist').then((res) => {
+      console.log(res)
+    })
+  }
   render() {
     return (
       <div>
