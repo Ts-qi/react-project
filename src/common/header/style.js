41d26dfe38bcd41d9f24fedee10fda79d80b2a75
tangqi <1024505996@qq.com>
2018-09-05 22:47:18 +0800

    完成头部动画优化

--- src/common/header/style.js
+++ src/common/header/style.js
@@ -42,18 +42,6 @@ export const NavItem = styled.div`
 export const SearchWrapper = styled.div `
     position: relative;
     float:left;
-    .slide-enter {
-        transition: all .2s ease-out;
-    }
-    .slide-enter-active {
-        width:240px;
-    }
-    .slide-exit {
-        transition: all .2s ease-out;
-    }
-    .slide-exit-active {
-        width:160px;
-    }
     .iconfont {
         position:absolute;
         right: 1px;
@@ -88,6 +76,18 @@ export const NavSearch = styled.input.attrs({
     }
     &.focused {
         width: 200px;
+    }
+    &.slide-enter {
+        transition: all .2s ease-out;
+    }
+    &.slide-enter-active {
+        width:200px;
+    }
+    &.slide-exit {
+        transition: all .2s ease-out;
+    }
+    &.slide-exit-active {
+        width:160px;
     }
 `;
 export const Addition = styled.div`
